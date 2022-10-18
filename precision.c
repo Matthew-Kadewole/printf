69f2f3802931ab82f4737dff82d8378bdc05e3f0
matthew <matthewkadewole@gmail.com>
2022-10-18 05:53:24 -0700

    updated comment

--- precision.c
+++ precision.c
@@ -4,7 +4,9 @@
  * get_precision - gets the precision from the format string
  * @s: the format string
  * @params: the parameters struct
+ *
  * @ap: the argument pointer
+ * 
  * Return: new pointer
 */
 
